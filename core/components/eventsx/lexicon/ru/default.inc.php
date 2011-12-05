fb7128a7923c8baa4789f0179db6c7efdda95837
Jeroen Kenters <jeroen@kenters.com>
2011-12-05 09:47:06 +0100

    Russian translation

---
+++ core/components/eventsx/lexicon/ru/default.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Default Russian Lexicon Entries for EventsX
+ *
+ * @package eventsx
+ * @subpackage lexicon
+ */
+$_lang['eventsx'] = 'EventsX';
+$_lang['eventsx.desc'] = 'Управление событиями';
+
+$_lang['eventsx.event'] = 'Cобытия';
+$_lang['eventsx.event.new'] = 'Новое событие';
+$_lang['eventsx.event.update'] = 'Обновление события';
+$_lang['eventsx.event.remove'] = 'Удалить событие';
+$_lang['eventsx.event.remove.confirm'] = 'Вы действительно хотите удалить это событие?';
+$_lang['eventsx.event.error.nf'] = 'События не найдены';
+$_lang['eventsx.event.name'] = 'Название';
+$_lang['eventsx.event.active'] = 'Активный';
+$_lang['eventsx.event.description'] = 'Описание';
+$_lang['eventsx.event.startdate'] = 'Дата начала';
+$_lang['eventsx.event.enddate'] = 'Дата окончания';
+$_lang['eventsx.event.location'] = 'Местоположение';
+$_lang['eventsx.event.location.street'] = 'Улица';
+$_lang['eventsx.event.location.pc'] = 'Индекс';
+$_lang['eventsx.event.location.city'] = 'Город';
+$_lang['eventsx.event.location.country'] = 'Страна';
+$_lang['eventsx.event.location.website'] = 'Веб-сайт';
